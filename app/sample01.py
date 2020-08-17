f1079a7d799e56eb0b7f1d2872d9f92e7f1f7caa
RottenFruits <s_ogawa@akarf.com>
2020-08-17 21:57:16 +0900

    update

--- app/sample01.py
+++ app/sample01.py
@@ -2,21 +2,57 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
+import math
 import sys
 
 @st.cache
 def read_data(path):
     df = pd.read_csv(path)
-    #df = df[df["type"] == 1]
     return df
 
 def add_widget():
     add_filter_data_row_number = st.sidebar.number_input(
         'Select a max number of show data num',
-        0, 1000, 100,
+        0, 10000, 10000,
         key = "1"
     )
     return add_filter_data_row_number
+
+
+def add_bar_histogram(df, columns, bin):
+    num_columns = len(columns)
+    plot_cols = math.ceil(num_columns / 10)
+    base = alt.Chart().mark_bar().encode().properties(
+        width = 300 / plot_cols,
+        height = 300 / plot_cols
+    ).interactive()
+    plot = alt.vconcat(data = df)
+    i = 0
+    row = alt.hconcat()
+    for x_encoding in columns:
+        if i % plot_cols == 0 and i != 0:
+            plot &= row
+            row = alt.hconcat()
+        if bin:
+            row |= base.encode(x = alt.X(x_encoding, bin = True), y = alt.Y('count()'))
+        else:
+            row |= base.encode(x = x_encoding, y='count()')
+        if plot_cols == 1:
+            plot &= row
+        i += 1
+    return(plot)
+
+def add_scatter_plot(df, columns):
+    scatter_columns = st.multiselect(
+    "Choose columns", list(columns), [columns[0], columns[1]]
+    )
+    if len(scatter_columns) != 2:
+        st.error("Please select two columns.")
+        return
+    plot = alt.Chart(df).mark_circle().encode(
+        x = scatter_columns[0], y = scatter_columns[1]
+        ).interactive()
+    return(plot)
 
 def main():
     path = sys.argv[1]
@@ -26,7 +62,7 @@ def main():
     """
     # Sample 1: Check data app
     
-    ## Data summary
+    ## Summary
     """
 
     '### Data shape:'
@@ -42,7 +78,7 @@ def main():
     df_types = df.dtypes
     df_types
 
-    '### Statistics summary:'
+    '### Statistics:'
     print_data_describe  = df.describe()
     print_data_describe
 
@@ -59,50 +95,22 @@ def main():
     int_float_columns = columns[(df_types == "int64") | (df_types == "float64")]
     category_columns = columns[~((df_types == "int64") | (df_types == "float64"))]
 
-    '### Histogram:'
-    base = alt.Chart().mark_bar().encode().properties(
-        width = 150,
-        height = 150
-    ).interactive()
-    h1 = alt.vconcat(data = df_filtered)
-    i = 0
-    row = alt.hconcat()
-    for x_encoding in int_float_columns:
-        if i % 4 == 0 and i != 0:
-            h1 &= row
-            row = alt.hconcat()
-        row |= base.encode(x = alt.X(x_encoding, bin = True), y='count()')
-        i += 1 
-    h1
+    # histogram
+    if len(int_float_columns) > 0:
+        '### Histogram:'
+        h = add_bar_histogram(df_filtered, int_float_columns, bin = True)
+        h
 
-    '### Bar plot:'
-    base2 = alt.Chart().mark_bar().encode().properties(
-        width = 150,
-        height = 150
-    ).interactive()
-    b = alt.vconcat(data = df_filtered)
-    i = 0
-    row = alt.hconcat()
-    for x_encoding in category_columns:
-        if i % 4 == 0 and i != 0:
-            b &= row
-            row = alt.hconcat()
-        row |= base2.encode(x = x_encoding, y = 'count()')
-        i += 1 
-    b
+    # bar
+    if len(category_columns) > 0:
+        '### Bar plot:'
+        b = add_bar_histogram(df_filtered, category_columns, bin = False)
+        b
 
-
-    '### Scatter plot:'
-    scatter_columns = st.multiselect(
-    "Choose columns", list(int_float_columns), [int_float_columns[0], int_float_columns[1]]
-    )
-    if len(scatter_columns) != 2:
-        st.error("Please select two columns.")
-        return
-    
-    p1 = alt.Chart(df_filtered).mark_circle().encode(
-        x = scatter_columns[0], y = scatter_columns[1]
-    )
-    p1
+    # scatter plot
+    if len(int_float_columns) > 2:
+        '### Scatter plot:'
+        p = add_scatter_plot(df_filtered, int_float_columns)
+        p
 
 main()
